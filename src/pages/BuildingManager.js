29d90ea15f71001067afc27ec324121f051f425c
Parker <66870782+ngu892@users.noreply.github.com>
2024-09-07 04:45:34 +0000

    BuldingManager page

---
+++ src/pages/BuildingManager.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function BuildingManager() {
+  return (
+    <div className="BuildingManager">
+      <div className="headerContainer">
+        <h1>THIS is the BuildingManager page</h1>
+      </div>
+    </div>
+  )
+}
+
+export default BuildingManager
