8ab6330bfadb384fd8fde68bd960851522bb562f
Y1h2a <163083675+Y1h2a@users.noreply.github.com>
2024-10-26 06:14:24 +0000

    Add viewing history feature

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders homepage header', () => {
-  render(<App />);
-  const headerElement = screen.getByText(/THIS IS THE HOMEPAGE/i);
-  expect(headerElement).toBeInTheDocument();
-});
