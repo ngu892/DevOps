43c516aabc2a4ea5f3f3f92c92e520069729ff7a
AK1116q <148340327+AK1116q@users.noreply.github.com>
2024-10-28 13:03:00 +0000

    add forbiddenwords

---
+++ src/assets/forbiddenWords.js
@@ -0,0 +1,32 @@
+
+const forbiddenWords = [
+    "abuse",
+    "idiot",
+    "stupid",
+    "hate",
+    "dumb",
+    "bastard",
+    "freak",
+    "suck",
+    "trash",
+    "loser",
+    "nigger",
+    "fuck",
+    "hell",
+    "jerk",
+    "moron",
+    "damn",
+    "shit",
+    "psycho",
+    "crap",
+    "jerk",
+    "fool",
+    "chingchong",
+    "asshole",
+   
+
+    
+  ];
+  
+  export default forbiddenWords;
+  
